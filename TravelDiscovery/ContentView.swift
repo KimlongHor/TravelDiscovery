9dc6b51ed8d65b189fdc2ec566821537afde587f
Kimlong Hor <kimlonghor11@gmail.com>
2021-07-26 18:42:13 -0500

    First commit

---
+++ TravelDiscovery/ContentView.swift
@@ -0,0 +1,239 @@
+//
+//  ContentView.swift
+//  TravelDiscovery
+//
+//  Created by horkimlong on 26/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                DiscoverCategoriesView()
+                PopularDestinationView()
+                PopularRestaurantsView()
+                TrendingCreatorsView()
+            }.navigationTitle("Discover")
+        }
+    }
+}
+
+// Trending places
+
+struct User: Hashable {
+    let name, imageName: String
+}
+
+struct TrendingCreatorsView: View {
+    let users: [User] = [
+        .init(name: "Amy Adams", imageName: "amy"),
+        .init(name: "Billy", imageName: "billy"),
+        .init(name: "Sam Smith", imageName: "sam")
+    ]
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(alignment: .top, spacing: 12) {
+                    ForEach(users, id: \.self) { user in
+                        VStack {
+                            Image(user.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(60)
+                            Text(user.name)
+                                .font(.system(size: 11, weight: .semibold))
+                                .multilineTextAlignment(.center)
+                        }
+                            .frame(width: 60)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+// -- end --
+
+// Popular place to eat
+
+struct Restaurant: Hashable {
+    let name, imageName: String
+}
+
+struct PopularRestaurantsView: View {
+    
+    let restaurant: [Restaurant] = [
+        .init(name: "Japan's Finest Tapas", imageName: "tapas"),
+        .init(name: "Bar & Grill", imageName: "bar_grill")
+    ]
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(restaurant, id: \.self) { restaurant in
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(restaurant.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .clipped()
+                                .cornerRadius(5)
+                                .padding(.leading, 6)
+                                .padding(.vertical, 6)
+                            
+                            VStack(alignment: .leading, spacing: 6) {
+                                HStack {
+                                    Text(restaurant.name)
+                                    Spacer()
+                                    Button(action: {}, label: {
+                                        Image(systemName: "ellipsis")
+                                            .foregroundColor(.gray)
+                                    })
+                                }
+                                    .padding(.trailing)
+                                
+                                HStack(alignment: .center) {
+                                    Image(systemName: "star.fill")
+                                    Text("4.7 • Sushi • $$")
+                                }
+                                Text("Tokyo, Japan")
+                            }.font(.system(size: 12, weight: .semibold))
+                        }
+                            .frame(width: 240)
+                            .background(Color(.init(white: 0.9, alpha: 1)))
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+// -- end --
+
+// Popular Destination
+
+struct Destination: Hashable {
+    let name, country, imageName: String
+}
+
+
+struct PopularDestinationView: View {
+    let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york")
+    ]
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text("Popular destinations")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(destinations, id: \.self) { destination in
+                        VStack(alignment: .leading, spacing: 0) {
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 125, height: 125)
+                                .cornerRadius(4)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
+                            
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                                .foregroundColor(.gray)
+                        }
+                            .background(Color(.init(white: 0.9, alpha: 1)))
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+// -- end --
+
+struct Category: Hashable {
+    let name, imageName: String
+}
+
+struct DiscoverCategoriesView: View {
+    
+    var categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live Events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "music.mic"),
+        .init(name: "History", imageName: "music.mic")
+    ]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories, id: \.self) { category in
+                    VStack {
+                        Image(systemName: category.imageName)
+                            .foregroundColor(.white)
+                            .frame(width: 64, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(64)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.init(top: 0, leading: 0, bottom: 5, trailing: 0))
+                        Text(category.name)
+                            .font(.system(size: 12, weight: .semibold))
+                    }.frame(width: 68)
+                }
+            }.padding(.horizontal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by horkimlong on 26/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
