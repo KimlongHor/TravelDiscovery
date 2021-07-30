6dc62be0f16c46817e576b579e1aea7fcc7d082e
Kimlong Hor <kimlonghor11@gmail.com>
2021-07-29 20:10:05 -0500

    Finished loading images from JSON in DiscoverCategoriesView

---
+++ TravelDiscovery/Models/Place.swift
@@ -0,0 +1,12 @@
+//
+//  Place.swift
+//  TravelDiscovery
+//
+//  Created by horkimlong on 29/7/21.
+//
+
+import Foundation
+
+struct Place: Decodable, Hashable {
+    let name, thumbnail: String
+}
